7ddf4715e2bda05eefba01ba25a678b032a599e1
Alireza <ialireza@Alirezas-MacBook-Air.local>
2026-05-10 21:58:57 +0330

    write my first test

---
+++ tests/intro.test.js
@@ -0,0 +1,16 @@
+import { describe, expect, it } from "vitest";
+import { max } from "../src/intro";
+
+describe("max", () => {
+  it("should return the first argument if it is greater", () => {
+    // Arrange
+    const a = 2;
+    const b = 1;
+    // Act
+    const result = max(a, b);
+    // Assert
+    expect(result).toBe(2);
+
+    // expect(max(2, 1)).toBe(2);
+  });
+});
