d4beff5089c7b1efa58feba91d40f4d9886c7707
Alireza <khanamani1998@gmail.com>
2026-05-11 08:49:12 +0330

    comment code

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -4,14 +4,14 @@ import { max } from "../src/intro";
 describe("max", () => {
   it("should return the first argument if it is greater", () => {
     // Arrange
-    const a = 2;
-    const b = 1;
+    // const a = 2;
+    // const b = 1;
     // Act
-    const result = max(a, b);
+    // const result = max(a, b);
     // Assert
-    expect(result).toBe(2);
+    // expect(result).toBe(2);
 
-    // expect(max(2, 1)).toBe(2);
+    expect(max(2, 1)).toBe(2);
   });
 
   it("should return the second argument if it is greater", () => {
