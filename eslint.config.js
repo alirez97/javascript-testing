385d14ebfaf152fd4b1c0af17a041cc881cbe7e4
Alireza <khanamani1998@gmail.com>
2026-05-18 22:26:29 +0330

    feat: add eslint-typescript

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,14 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.browser },
+  },
+  ...tseslint.configs.recommended,
 ]);
